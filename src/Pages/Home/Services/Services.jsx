ef47f2196100135b4de413d579695914ce28e8eb
developerabdullah-me <abdullahtalha9999@gmail.com>
2022-05-01 21:51:11 +0600

    sho length from dat abase

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-
-
+import useServices from '../../Sheare/Usedata/UseData';
 
 
 const Services = () => {
+    const [services,setServices]=useServices()
     return (
         <div>
-
-
-
+   
+   <h1>services{services.length}</h1>
         </div>
     );
 };
