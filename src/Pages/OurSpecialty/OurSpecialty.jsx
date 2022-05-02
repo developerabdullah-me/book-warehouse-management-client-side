5a274242e2465a5aa60a4fb4b12b6ebda2b24c4f
developerabdullah-me <abdullahtalha9999@gmail.com>
2022-05-03 03:12:37 +0600

    Best Sellers products

--- src/Pages/OurSpecialty/OurSpecialty.jsx
+++ src/Pages/OurSpecialty/OurSpecialty.jsx
@@ -1,15 +1,9 @@
 
 import React, { useRef, useState } from "react";
-// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
-
 import "./OurSpecialty.css";
-
-// import required modules
 import { Pagination } from "swiper";
 import useServices from "../Sheare/Usedata/UseData";
 
@@ -17,9 +11,10 @@ import useServices from "../Sheare/Usedata/UseData";
 const OurSpecialty = () => {
     const [services,setServices]=useServices()
     return (
-        <div>
-            
-          <>
+        <div className="mt-16">
+             <h1 className="text-4xl text-center">Best Sellers</h1>
+             <p className="text-center py-1">This product is top sells in the year of {(new Date().getFullYear())}</p>
+          <div>
       <Swiper
         slidesPerView={3}
         spaceBetween={30}
@@ -30,18 +25,19 @@ const OurSpecialty = () => {
         className="mySwiper"
       >
       { 
-            services.slice(0,6).map(service =>  <SwiperSlide className="border mt-11 mb-10" service={service._id}>
+            services.slice(0,6).map(service =>  <SwiperSlide className="border mt-11 mb-10  hover:shadow-lg cursor-pointer" service={service._id}>
                
                <div className="pb-16">
                <img src={service.img} alt="" />
                <p>{service.name}</p>
-
+               <p>$ <small>{service.price}</small></p>
+                
                </div>
                 
                 </SwiperSlide>)
             }
       </Swiper>
-    </>
+    </div>
         </div>
     );
 };
